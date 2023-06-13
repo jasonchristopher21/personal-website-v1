700de667a25a8fead6ec6bbf1e1cf5ac8141fb13
Jason Christopher <jason.christopher@u.nus.edu>
2023-06-13 23:36:16 +0800

    Add About page

---
+++ components/About.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+    <div class="flex flex-col" id="about">
+        <div class="flex gap-5">
+            <span class="font-monospace font-bold text-[34px] text-blue-200 my-auto">01.</span>
+            <span class="font-gilroy font-extrabold text-[48px] text-white">About me</span>
+        </div>
+        <div class="flex flex-col gap-10 xl:w-[60%] font-metropolis text-[24px] text-white mt-10 text-justify">
+            <p>
+                I am a penultimate computer science undergraduate at the National University of Singapore. I am passionate
+                about building software that solves real-world problems and improves the lives of others.
+            </p>
+            <p>
+                During my internship, I have worked around <span class="font-bold text-green">Fullstack Development</span>, handling both frontend and backend endpoint management.
+                Most of my work revolve around <span class="font-bold text-yellow">Frontend Development</span> and <span
+                    class="font-bold text-yellow">UI/UX Design</span>, which I polish consistently through projects and
+                participating in hackathons.
+            </p>
+            <p>
+                I am also interested in <span class="font-bold text-blue-100">Machine Learning</span>, <span
+                    class="font-bold text-blue-100">Natural Language Processing</span> and <span
+                    class="font-bold text-blue-100">Computer Security</span>. I have taken modules in these areas and have
+                worked on projects that involve them, such as building a search engine by prioritising query relevance.
+        </p>
+    </div>
+    <div class="h-[100px]"></div>
+</div></template>
