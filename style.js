e2364d4c319525239d69c9c1a530f78e73aaccfd
Jason Christopher <jason.christopher@u.nus.edu>
2023-06-14 00:48:09 +0800

    Bugfix: Fix quotation format in style.js

--- style.js
+++ style.js
@@ -1,9 +1,9 @@
 const styles = {
-    heading1: `font-gilroy font-extrabold text-white text-[48px]`,
-    heading2: `font-gilroy font-extrabold text-white text-[40px]`,
-    heading3: `font-gilroy font-extrabold text-white text-[36px]`,
-    heading4: `font-gilroy font-extrabold text-white text-[32px]`,
-    heading5: `font-gilroy font-extrabold text-white text-[24px]`,
+    heading1: "font-gilroy font-extrabold text-white text-[48px]",
+    heading2: "font-gilroy font-extrabold text-white text-[40px]",
+    heading3: "font-gilroy font-extrabold text-white text-[36px]",
+    heading4: "font-gilroy font-extrabold text-white text-[32px]",
+    heading5: "font-gilroy font-extrabold text-white text-[24px]",
 
     paragraph: "font-metropolis font-normal text-white text-[20px]",
     codeParagraph: "font-monospace font-medium text-[16px]",
